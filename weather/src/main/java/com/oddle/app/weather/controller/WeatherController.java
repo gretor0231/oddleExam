e9c2d1ce6fc4ceceb7a937eee5193943a5a20562
gretor0231 <gretor0231@gmail.com>
2022-02-21 17:59:48 +0800

    [oddle] added some comments for controller

--- weather/src/main/java/com/oddle/app/weather/controller/WeatherController.java
+++ weather/src/main/java/com/oddle/app/weather/controller/WeatherController.java
@@ -20,26 +20,35 @@ public class WeatherController {
         this.weatherService = weatherService;
     }
 
+//    get the weather information from openWeather by cityName
+//    the Url example would be http://localhost:8080/weather/shanghai
     @GetMapping("/{cityName}")
     public Map<String, Object> getWeathers(@PathVariable String cityName)  {
         CurrentWeather currentWeather = weatherService.findByCity(cityName);
         return Collections.singletonMap("message", "the weather of " + cityName +" is : " + currentWeather.toString());
     }
+//    get the weather information from mySql database
+//    the Url example would be http://localhost:8080/weather/list
     @GetMapping("/list")
     public Map<String, Object> getAllWeathers() {
         List<CurrentWeather> listOfCurrentWeather = weatherService.findAllWeather();
         return Collections.singletonMap("message", "the all weather information's are : " + listOfCurrentWeather);
     }
+//    save the weather information from openWeather by cityName
+//    the Url example would be http://localhost:8080/weather/shanghai
     @PostMapping("/{cityName}")
     public Map<String, Object> postWeathers(@PathVariable String cityName) {
         weatherService.save(cityName);
         return Collections.singletonMap("message", "the weather of " + cityName +" already saved. ");
     }
+//    delete the weather information from mySql database by id
+//    the Url example would be http://localhost:8080/weather/1
     @DeleteMapping("/{id}")
     public Map<String, Object> deleteWeathers(@PathVariable String id) {
         return Collections.singletonMap("message", "the id already deleted : " + weatherService.deleteByID(id));
     }
-
+//    update the weather information from mySql database by id
+//    the Url example would be http://localhost:8080/weather/1
     @PutMapping("/{id}")
     public Map<String, Object> putWeathers(@PathVariable String id) {
          return Collections.singletonMap("message", "the id already updated : " + weatherService.update(id));
